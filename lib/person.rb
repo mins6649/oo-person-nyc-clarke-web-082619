8d218cb35742ee3d41ae7920d5a3472b37ff9192
mins6649 <74225425+mins6649@users.noreply.github.com>
2021-02-24 20:59:19 -0500

    Update person.rb

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,94 @@
 # your code goes here
+class Person
+
+
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+
+
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+
+        # if happiness >= 10
+        #     @happiness = 10
+        # else
+        #     @happiness = happiness
+        # end
+    end
+
+    def happiness=(happiness)
+        if happiness >= 10
+            @happiness = 10
+        elsif happiness <= 0
+            @happiness = 0
+        else
+            @happiness = happiness
+        end
+    end
+
+    def hygiene=(hygiene)
+        if hygiene >= 10
+            @hygiene = 10
+        elsif hygiene <= 0
+            @hygiene = 0
+        else
+            @hygiene = hygiene
+        end
+    end
+
+    def happy?
+        if self.happiness > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def clean?
+        if self.hygiene > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def get_paid(salary)
+        self.bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness +=2
+        return '♪ another one bites the dust ♫'
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        if topic == 'politics'
+            self.happiness -= 2
+            person.happiness -= 2
+            return 'blah blah partisan blah lobbyist'
+        elsif topic == "weather"
+            self.happiness += 1
+            person.happiness += 1
+            return 'blah blah sun blah rain'
+        else 
+            return 'blah blah blah blah blah'
+        end
+    end    
+end
